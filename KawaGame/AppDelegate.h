5c0b1bbf625ac902b339b471a1da513389f3a5a6
gyf5267 <gyf5267218@163.com>
2015-10-14 21:43:22 +0800

    Initial Commit

---
+++ KawaGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KawaGame
+//
+//  Created by nate on 15/10/14.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KawaGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KawaGame
+//
+//  Created by nate on 15/10/14.
+//  Copyright (c) 2015年 nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
